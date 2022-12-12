9b6c1dfb3c5a2e66ea38a6bdd660e71753a1c7cc
joseph mwanzia <makamwanzia@gmail.com>
2022-12-13 02:21:15 +0300

    pushed labs

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 function addingEventListener() {
+    const input = document.getElementById('button');
+
+    function clickAlert() {
+        console.log();('I was clicked!');
+    }
+
+    input.addEventListener('click', clickAlert);
 }
+// addingEventListener()
